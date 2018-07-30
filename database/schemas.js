bfce78f46d01e89bb712cc35cc9e080a4f7ac220
Roman Gorelik <36119654+romangorelik@users.noreply.github.com>
2018-07-30 14:17:17 -0400

    Update schemas.js

--- database/schemas.js
+++ database/schemas.js
@@ -8,14 +8,22 @@ let userFavsSchema = mongoose.Schema({
   time : { type : Date, default: Date.now }
 })
 
+// This schema is for saving a favorite message by username. It takes in a username, a favorite messages,
+// and from who it was sent.
+
 let usersSchema = mongoose.Schema({
   username: {type: String, unique: true, required: true},
 })
+
+// This schema is used for saving and retrieving users in the database.
 
 let friendSchema = mongoose.Schema({
   username: {type: String, unique: false},
   friend: {type: String, _id: false}
 })
+
+// This schema is used to save a friend by both your username and their username. This way you can
+// differentiate what users have what friends.
 
 let Favorites = db.model('Favorites', userFavsSchema)
 let Users = db.model('Users', usersSchema)
